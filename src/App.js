ee9bd793ed6a25241e60e4830cff8a5306d07970
quokkaDH <123261373+quokkaDH@users.noreply.github.com>
2023-01-22 11:23:02 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import logo from './img/test_main.png';
+import styled from "styled-components";
+import Button from "./button";
+import { useState } from 'react';
 
 function App() {
+
+  let [thumb, thumbPlus] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className='' alt='React' />
-          UriSystem Hompage dev1.0
+        <img src={logo} onClick={()=>{ thumbPlus(0)}} className='' alt='React' />
+        <text>UriSystem Hompage dev1.0</text>
+        &nbsp;
+        <span onClick={ ()=>{ thumbPlus(thumb + 1)}}>👍{thumb}</span>
+        {/* <text><Button primary>test</Button></text>*/}
       </header>
     </div>
   );
